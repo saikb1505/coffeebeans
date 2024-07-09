03e005ff45a86a093fa94f8ea1157b7db54813c5
Sai Bandaru <saibandaru@ALIPL1730.local>
2024-07-09 15:00:58 +0530

    With valid API key we can add more scenarios in the tests

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe EventsController, type: :controller do
     it 'creates Event A', :vcr do
       post :create_event_a
       expect(response).to redirect_to(root_path)
+      # with a valid API key we can add mode tests to verify the functionality
       expect(flash[:notice]).to eq('Event A created')
     end
   end
@@ -17,6 +18,7 @@ RSpec.describe EventsController, type: :controller do
     it 'creates Event B and sends email', :vcr do
       post :create_event_b
       expect(response).to redirect_to(root_path)
+      # with a valid API key we can add mode tests to verify the functionality
       expect(flash[:notice]).to eq('Event B created and email sent')
     end
   end
